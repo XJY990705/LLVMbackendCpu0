7e4fd79bef39fdd2fed2b270725bd6a9ec31a300
XJY990705 <xujiayue21@mails.ucas.ac.cn>
2022-11-03 11:48:20 +0800

    "3.1 TargetMachine structure"

--- llvm/lib/Target/Cpu0/Cpu0TargetMachine.h
+++ llvm/lib/Target/Cpu0/Cpu0TargetMachine.h
@@ -5,13 +5,82 @@
 // This file is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
 //
-//===----------------------------------------------------------------------===
+//===----------------------------------------------------------------------===//
 //
 // This file declares the Cpu0 specific subclass of TargetMachine.
 //
-//===----------------------------------------------------------------------===
+//===----------------------------------------------------------------------===//
 
 #ifndef LLVM_LIB_TARGET_CPU0_CPU0TARGETMACHINE_H
 #define LLVM_LIB_TARGET_CPU0_CPU0TARGETMACHINE_H
+
+#include "MCTargetDesc/Cpu0ABIInfo.h"
+#include "Cpu0Subtarget.h"
+#include "llvm/ADT/Optional.h"
+#include "llvm/ADT/StringMap.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/CodeGen.h"
+#include "llvm/CodeGen/Passes.h"
+#include "llvm/CodeGen/SelectionDAGISel.h"
+#include "llvm/CodeGen/TargetFrameLowering.h"
+#include "llvm/Target/TargetMachine.h"
+#include <memory>
+
+namespace llvm {
+class formatted_raw_ostream;
+class Cpu0RegisterInfo;
+
+class Cpu0TargetMachine : public LLVMTargetMachine {
+  bool isLittle;
+  std::unique_ptr<TargetLoweringObjectFile> TLOF;
+  Cpu0ABIInfo ABI;
+  Cpu0Subtarget DefaultSubtarget;
+
+  mutable StringMap<std::unique_ptr<Cpu0Subtarget>> SubtargetMap;
+public:
+  Cpu0TargetMachine(const Target &T, const Triple &TT, StringRef CPU,
+                    StringRef FS, const TargetOptions &Options,
+                    Optional<Reloc::Model> RM, Optional<CodeModel::Model> CM,
+                    CodeGenOpt::Level OL, bool JIT, bool isLittle);
+  ~Cpu0TargetMachine() override;
+
+  const Cpu0Subtarget *getSubtargetImpl() const {
+    return &DefaultSubtarget;
+  }
+
+  // Can use this interface to fetch subtarget
+  const Cpu0Subtarget *getSubtargetImpl(const Function &F) const override;
+
+  // Pass Pipeline Configuration
+  TargetPassConfig *createPassConfig(PassManagerBase &PM) override;
+
+  TargetLoweringObjectFile *getObjFileLowering() const override {
+    return TLOF.get();
+  }
+  bool isLittleEndian() const { return isLittle; }
+  const Cpu0ABIInfo &getABI() const { return ABI; }
+};
+
+// This is big endian type Cpu032 target machine.
+class Cpu0ebTargetMachine : public Cpu0TargetMachine {
+  virtual void anchor();
+public:
+  Cpu0ebTargetMachine(const Target &T, const Triple &TT, StringRef CPU,
+                      StringRef FS, const TargetOptions &Options,
+                      Optional<Reloc::Model> RM,
+                      Optional<CodeModel::Model> CM,
+                      CodeGenOpt::Level OL, bool JIT);
+};
+// This is default little endian Cpu032 target machine.
+class Cpu0elTargetMachine : public Cpu0TargetMachine {
+  virtual void anchor();
+public:
+  Cpu0elTargetMachine(const Target &T, const Triple &TT, StringRef CPU,
+                      StringRef FS, const TargetOptions &Options,
+                      Optional<Reloc::Model> RM,
+                      Optional<CodeModel::Model> CM,
+                      CodeGenOpt::Level OL, bool JIT);
+};
+} // End llvm namespace
 
 #endif
